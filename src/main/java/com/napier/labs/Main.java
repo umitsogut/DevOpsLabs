2a5073e92bcc7ab43c89c33e543f86d30b69f75e
Umit-Sogut <40408868@live.napier.ac.uk>
2025-10-09 06:55:58 +0100

    Prints salaries
    Signed-off-by: Umit-Sogut <40408868@live.napier.ac.uk>

--- src/main/java/com/napier/labs/Main.java
+++ src/main/java/com/napier/labs/Main.java
@@ -20,6 +20,8 @@ public class Main {
         ArrayList<Employee> employees = a.getAllSalaries();
         // Test the size of the returned data - should be 240124
         System.out.println(employees.size());
+        // Prints salaries
+        a.printSalaries(employees);
         // Disconnect from database
         a.disconnect();
     }
@@ -166,6 +168,24 @@ public class Main {
             System.out.println(e.getMessage());
             System.out.println("Failed to get salary details");
             return null;
+        }
+    }
+
+    /**
+     * Prints a list of employees.
+     * @param employees The list of employees to print.
+     */
+    public void printSalaries(ArrayList<Employee> employees)
+    {
+        // Print header
+        System.out.println(String.format("%-10s %-15s %-20s %-8s", "Emp No", "First Name", "Last Name", "Salary"));
+        // Loop over all employees in the list
+        for (Employee emp : employees)
+        {
+            String emp_string =
+                    String.format("%-10s %-15s %-20s %-8s",
+                            emp.emp_no, emp.first_name, emp.last_name, emp.salary);
+            System.out.println(emp_string);
         }
     }
 }
