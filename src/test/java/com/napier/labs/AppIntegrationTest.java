0876dacac3c24531382cddabc3fbc69b6d654967
Umit-Sogut <40408868@live.napier.ac.uk>
2025-12-10 23:09:26 +0000

    addEmployee method added!

--- src/test/java/com/napier/labs/AppIntegrationTest.java
+++ src/test/java/com/napier/labs/AppIntegrationTest.java
@@ -28,4 +28,18 @@ public class AppIntegrationTest
         assertEquals(emp.first_name, "Ronghao");
         assertEquals(emp.last_name, "Garigliano");
     }
+
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
 }
