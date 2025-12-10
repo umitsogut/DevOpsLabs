d7bb7566b7c2999d9ab86b3b90e6a0baaf176460
Umit-Sogut <40408868@live.napier.ac.uk>
2025-12-10 22:19:34 +0000

    GitHub workflow updated!

---
+++ src/test/java/com/napier/labs/AppIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.napier.labs;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    static Main app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new Main();
+        app.connect("localhost:33060", 30000);
+
+    }
+
+    @Test
+    void testGetEmployee()
+    {
+        Employee emp = app.getEmployee(255530);
+        assertEquals(emp.emp_no, 255530);
+        assertEquals(emp.first_name, "Ronghao");
+        assertEquals(emp.last_name, "Garigliano");
+    }
+}
